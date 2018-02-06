fe360c7965fe2f67300626359b0152c434ae4016
Kaitrina High <kaitrinahigh@zipcoders-MacBook-Pro-3.local>
2018-02-06 15:46:11 -0500

    first commit

---
+++ src/main/java/com/zipcodewilmington/MainApplication.java
@@ -0,0 +1,11 @@
+package com.zipcodewilmington;
+
+public class MainApplication {
+    //psmv short cut to create main
+    public static void main(String[] args) {
+        //sout short cut to create
+        //shift f10 hot key for run
+        //command 1 hide task payne
+        System.out.println("Hello World");
+    }
+}
